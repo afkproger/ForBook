1b20045ec8907b3e70ec080456af6aef145fd571
afkproger <126967895+afkproger@users.noreply.github.com>
2023-10-30 11:44:03 +0300

    Ильяс лох

--- TreeSorting/src/Main.java
+++ TreeSorting/src/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         TreeSort<Integer> sort = new TreeSort<>();
         System.out.println(sort.sort(List.of(1, 5, 1, 8, 9, 5, 3, 24, 6, 2, 5262, 62, 2, 734, 25, 126, 765, 78, 87, 3, 625, 242, 26, 373, 73, 452, 5262, 62)));
-        System.out.println("Hello");
     }
 }
